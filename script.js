aa9db50634522cfa1421145e8731def59e0d84e0
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-05-30 08:28:16 +0545

    local storage added

--- script.js
+++ script.js
@@ -3,28 +3,58 @@ let addButton = document.querySelector(".add-button");
 let todoList = document.querySelector(".todo-item");
 let delButt = document.querySelector(".del-button");
 
-const handleAddButton = () => {
-  let todoItem = userInput.value;
-  console.log(`${todoItem} was added`);
-
-  const newList = document.createElement("li");
-  newList.textContent = todoItem;
+let todolists =[];
+const getTodolistFromLocal =() =>{
+     return JSON.parse(localStorage.getItem("TodoList"))
+}
+const addTotoListLocalStorage=(todolists)=>{
+  return localStorage.setItem('TodoList', JSON.stringify(todolists));
+}
+const addTodoDynamic =(currElem)=>{
+   const newList = document.createElement("li");
+  newList.textContent = currElem;
   newList.classList.add("list-item");
   let delButton = document.createElement("button");
   delButton.textContent = "Delete";
   delButton.classList.add("del-button");
   newList.append(delButton);
   todoList.appendChild(newList);
+ 
+}
+todolists = getTodolistFromLocal() || [];
+const handleAddButton = () => {
+  let todoItem = userInput.value.trim();
+  console.log(`${todoItem} was added`);
+  if (todoItem!=="" && !todolists.includes(todoItem)){
+  todolists.push(userInput.value);
+  todolists=[...new Set(todolists)];
+  console.log(todolists);
+  localStorage.setItem("TodoList", JSON.stringify(todolists));
+
+  addTodoDynamic(todoItem);}
   userInput.value = "";
 };
+ const showTodoList=()=>{
+    todolists.forEach((currElem)=>{
+      addTodoDynamic(currElem);    })
+ }
+ showTodoList();
 const handleDelButton=(e)=>{
     console.log(e.target);
-    if (e.target && e.target.matches("button.del-button")){
+    const todoToRemove = e.target;
+    let todoListContent = todoToRemove.closest('li').innerText;
+    console.log(todoListContent);
+    todolists = todolists.filter((curTodo)=>{
+      return curTodo!= todoListContent.toLowerCase();
+    });
+    addTotoListLocalStorage(todolists);
+    if (todoToRemove && todoToRemove.classList.contains(".del-button")){
         const li =e.target.closest('li');
         if (li) li.remove();
     }
+   }
  
   
-}
+
 addButton.addEventListener("click", handleAddButton);
 todoList.addEventListener('click', handleDelButton);
