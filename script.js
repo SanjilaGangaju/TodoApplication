4ef66be19da09f6333f1b8f8b9aff59786f956bb
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-05-30 21:55:41 +0545

    code improvement

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
+// Dom Selections
 let userInput = document.querySelector(".user-input");
 let addButton = document.querySelector(".add-button");
-let todoList = document.querySelector(".todo-item");
-let delButt = document.querySelector(".del-button");
+let todoList = document.querySelector(".todo-list");
 
-let todolists =[];
-const getTodolistFromLocal =() =>{
-     return JSON.parse(localStorage.getItem("TodoList"))
-}
-const addTotoListLocalStorage=(todolists)=>{
-  return localStorage.setItem('TodoList', JSON.stringify(todolists));
-}
-const addTodoDynamic =(currElem)=>{
-   const newList = document.createElement("li");
+// local storage functions
+const getTodolistFromLocal = () => {
+  return JSON.parse(localStorage.getItem("TodoList"));
+};
+const addTodoListLocalStorage = (todolists) => {
+  return localStorage.setItem("TodoList", JSON.stringify(todolists));
+};
+
+// Dynamic DOM insertion function
+const addTodoDynamic = (currElem) => {
+  const newList = document.createElement("li");
   newList.textContent = currElem;
   newList.classList.add("list-item");
   let delButton = document.createElement("button");
@@ -19,43 +21,48 @@ const addTodoDynamic =(currElem)=>{
   delButton.classList.add("del-button");
   newList.append(delButton);
   todoList.appendChild(newList);
- 
-}
+};
+let todolists = [];
 todolists = getTodolistFromLocal() || [];
+
+// Add Button Click Handler
 const handleAddButton = () => {
   let todoItem = userInput.value.trim();
-  console.log(`${todoItem} was added`);
-  if (todoItem!=="" && !todolists.includes(todoItem)){
-  todolists.push(userInput.value);
-  todolists=[...new Set(todolists)];
-  console.log(todolists);
-  localStorage.setItem("TodoList", JSON.stringify(todolists));
-
-  addTodoDynamic(todoItem);}
+  if (todoItem !== "" && !todolists.includes(todoItem)) {
+    todolists.push(userInput.value);
+    // todolists=[...new Set(todolists)];
+    addTodoListLocalStorage(todolists);
+    addTodoDynamic(todoItem);
+  }
   userInput.value = "";
 };
- const showTodoList=()=>{
-    todolists.forEach((currElem)=>{
-      addTodoDynamic(currElem);    })
- }
- showTodoList();
-const handleDelButton=(e)=>{
-    console.log(e.target);
-    const todoToRemove = e.target;
-    console.log(todoToRemove);
-     if (todoToRemove && todoToRemove.classList.contains("del-button")){
-        const li = todoToRemove.closest('li');
-        if (li) li.remove();
+
+// Display Todos on Page Load
+const showTodoList = () => {
+  todolists.forEach((currElem) => {
+    addTodoDynamic(currElem);
+  });
+};
+showTodoList();
+// Delete Button Click Handler
+const handleDelButton = (e) => {
+  const todoToRemove = e.target;
+  if (todoToRemove && todoToRemove.classList.contains("del-button")) {
+    const li = todoToRemove.closest("li");
+
+    if (li) {
+      const todoText = li.childNodes[0].nodeValue.trim();
+      li.remove();
+      todolists = todolists.filter((curTodo) => {
+        return curTodo !== todoText.toLowerCase();
+      });
+      addTodoListLocalStorage(todolists);
     }
-    let todoListContent = todoToRemove.closest('li').innerText;
-    todolists = todolists.filter((curTodo)=>{
-      return curTodo!= todoListContent.toLowerCase();
-    });
-    addTotoListLocalStorage(todolists);
-   
-   }
- 
-  
+  }
+};
 
 addButton.addEventListener("click", handleAddButton);
-todoList.addEventListener('click', handleDelButton);
+todoList.addEventListener("click", handleDelButton);
+userInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") handleAddButton();
+});
